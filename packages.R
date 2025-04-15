488b4bcf589c8a98d180d72614fd5351b886e61d
Lisa Eash <leash@pointblue.org>
2025-04-15 10:30:30 -0700

    Create packages.R script for all packages needed in AgCDataCompile.R

---
+++ packages.R
@@ -0,0 +1,10 @@
+# Title: packages.R
+# Author: Lisa Eash
+# Date created: 20250402
+# Date updated: 20250414
+
+suppressPackageStartupMessages({
+  library(googlesheets4)
+  library(dplyr)
+  library(tidyr)
+})
