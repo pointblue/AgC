5368095bff78b645ee812c7540137606844d67d0
Lisa Eash <leash@pointblue.org>
2025-06-27 11:26:16 -0700

    Add readxl packages

--- packages.R
+++ packages.R
@@ -1,10 +1,11 @@
 # Title: packages.R
 # Author: Lisa Eash
 # Date created: 20250402
-# Date updated: 20250604
+# Date updated: 20250627
 
 suppressPackageStartupMessages({
   library(dplyr)
   library(tidyr)
   library(stringr)
+  library(readxl)
 })
