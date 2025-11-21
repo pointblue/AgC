7082fdfc7c8bcef8005de679774cb3a9f7da448f
Lisa Eash <leash@pointblue.org>
2025-11-21 09:33:00 -0800

    Fix outliers in Raca function

--- functions.R
+++ functions.R
@@ -380,8 +380,8 @@ reg_baseline <- function(polygon, #Specify polygon of project
   #Remove outliers
   BD_out <- boxplot.stats(RACA_res$BD)$out
   SOC_out <- boxplot.stats(RACA_res$SOC_perc)$out
-  RACA_res[!RACA_res$BD %in% BD_out, ]$BD <-NA
-  RACA_res[!RACA_res$SOC_perc %in% SOC_out, ]$SOC_perc <-NA
+  RACA_res[RACA_res$BD %in% BD_out, ]$BD <-NA
+  RACA_res[RACA_res$SOC_perc %in% SOC_out, ]$SOC_perc <-NA
   return(RACA_res)
   }
 
