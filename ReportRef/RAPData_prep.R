ace3125e27727c4ada2ab7b6f43a212179ae9ea3
avaloncookpb <acook@pointblue.org>
2026-03-31 10:22:01 -0700

    Improved organization for reporting on metrics not in ag-c database

---
+++ ReportRef/RAPData_prep.R
@@ -0,0 +1,18 @@
+#RAP Bareground data prep for land steward reports
+
+library(rapr)
+library(tigris)
+
+border.c <- st_transform(border, 4269) #this is the CRS needed for tigris
+counties_sf <- counties(cb = TRUE, class = "sf") %>% 
+  st_transform(st_crs(border.c))  # match CRS
+county <- counties_sf %>%
+  st_filter(border.c, .predicate = st_intersects)
+
+rap_region<-get_rap(county,
+                    product = "vegetation-cover",
+                    years = as.numeric(tp_lookup$year_label[length(tp_lookup$year_label)]), #takes the most recent year
+                    verbose = FALSE
+)
+
+rap_region_avg<-mean(terra::values(rap_region$bare_ground))
